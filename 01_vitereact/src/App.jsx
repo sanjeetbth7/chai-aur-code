a582b310af4b3436be9c5c10d77c1b93c78ee3c8
Sanjeet Kumar <sanjeetbth7@gmail.com>
2024-02-24 12:44:08 +0530

    day 1 React basics using vite

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Cricket from './Cricket.jsx'
+const App =(()=> {
+  return (
+    <>
+      <h1>Chai aur React | Sanjeet Kumar</h1>
+      <p>Started with create-react-app(an utility software) and learning why we should use vite instead of create-react-app . Vite is bundler.</p>
+      <Cricket />
+    </>
+  );
+});
+
+export default App;
---
+++ 01_vitereact/src/Cricket.jsx
@@ -0,0 +1,8 @@
+
+const Cricket = (()=>{
+    return(
+        <h1>I love to play Cricket.</h1>
+    );
+})
+
+export default Cricket;
